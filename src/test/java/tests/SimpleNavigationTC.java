62942da35f2aa8e48d0e4ef486ee08632e22bd4a
Vinayaka B Shetty <vinayakasjce@gmail.com>
2024-07-21 18:51:32 +0530

    added nav test case

---
+++ src/test/java/tests/SimpleNavigationTC.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SimpleNavigationTC {
+	@Test
+	public void navToGoogle() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://google.com");
+		Thread.sleep(6000);
+		driver.quit();
+	}
+}
